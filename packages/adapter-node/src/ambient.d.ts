53648856e1525f49904331c8616b37f1809d9ea2
senti <brianmv@uci.edu>
2023-07-02 12:08:56 -0700

    refactor: convert to monorepo

---
+++ packages/adapter-node/src/ambient.d.ts
@@ -0,0 +1,28 @@
+declare module 'HANDLER' {
+  export const handler: import('polka').Middleware;
+}
+
+declare module 'MANIFEST' {
+  import { SSRManifest } from '@sveltejs/kit';
+
+  export const manifest: SSRManifest;
+  export const prerendered: Set<string>;
+}
+
+declare module 'SERVER' {
+  export { Server } from '@sveltejs/kit';
+}
+
+interface ImportMeta {
+  SERVER_DIR: string;
+  ENV_PREFIX?: string;
+}
+
+declare namespace App {
+  export interface Platform {
+    /**
+     * The original Node request object (https://nodejs.org/api/http.html#class-httpincomingmessage)
+     */
+    req: import('http').IncomingMessage;
+  }
+}
